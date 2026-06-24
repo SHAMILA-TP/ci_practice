8cbdfe2cb43e464d1b18939cc3dba84a1b97bc55
SHAMILA-TP <shamilatps5@gmail.com>
2026-06-24 18:45:47 +0530

    main | Break a test intentionally

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,8 +4,8 @@ RSpec.describe Calculator do
   let(:calc) {Calculator.new}
   describe "Add" do
     it 'adds two numbers' do
-      p "fsfs"
-      expect(calc.add(10, 20)).to eq(30)
+      # expect(calc.add(10, 20)).to eq(30)
+      expect(calc.add(10, 20)).to eq(0)
     end
   end
 end
